c8617aa713bc3a9557d71c7ae60066ba7be225ea
souravdalal-coder <63602236+souravdalal-coder@users.noreply.github.com>
2021-05-07 15:41:03 +0530

    Update VehicleManagementAppApplication.java

--- src/main/java/com/cg/ovms/VehicleManagementAppApplication.java
+++ src/main/java/com/cg/ovms/VehicleManagementAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.cg.ovms")
 public class VehicleManagementAppApplication {
-
+//main method
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleManagementAppApplication.class, args);
 	}
